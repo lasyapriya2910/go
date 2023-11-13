e0f2fd29e8c25beb17bb9764f457ed27b9054fa3
Lasya Priya <106594507+lasyapriya2910@users.noreply.github.com>
2023-11-13 22:21:26 +0530

    Create wk6.go

---
+++ wk6.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main() {
+fmt.Print("Enter First String: ")
+to display output in same line
+var first string
+fmt.Scanln(&first)
+user
+fmt.Print("Enter Second String: ")
+var second string
+fmt.Scanln(&second)
+fmt.Print(first + second)
+string
+}
